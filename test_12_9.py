f855304ad76a0424a9a25e720a5d99b21d87cee0
fg 14-kl <3514596559@qq.com>
2022-12-10 22:28:40 +0800

    first commit

---
+++ test_12_9.py
@@ -0,0 +1,105 @@
+#print("hollo world")#dffcvcvcc
+#print("hahahaha")
+#print("vvvvvvvvvvvv")
+
+#for i in range (0,10):
+ #   print("i = %d" %(i))
+    
+#r=3
+
+#print("请输入N的值")    
+#N = int (input())
+
+#for i in range(0,N):
+#    r = r*2
+
+#print("%d年后，兔子的数量为%d"%(N,r))  
+
+#a =set()
+
+#b='abcd'
+#print(b)
+
+#c=[1,2,3]
+#test = set(c)
+#print(test)
+
+#d=(1,2,'abc')
+#test=set(d)
+#print(test)
+
+#e = {'a':1,'b':2,'c':3}
+#test = set(e)
+#print(test)
+
+#test = {1,2,'abc',567,8}
+#for item in test:
+#    print(item)
+    
+#nums = {1,2,3}
+#nums.add(6)
+#nums.remove(1)
+#print(nums)
+    
+#a={1,2,3,4}
+#b={3,4,5,6}
+
+#print(a&b)
+#print(a.intersection(b))
+
+#print(a|b)
+#print(a.union(b))
+
+#print(a-b)
+#print(a.difference(b))
+
+#print(b-a)
+#print(b.difference(a))
+#print(b.difference(b))
+
+#num1 = int(input('输入班级1的学生数量：'))
+
+#class1 = set()
+#for i in range(0,num1):
+#    name = input('输入学生%d姓名'%(i+1))
+ #   class1.add(name)
+    
+#num2=int(input('输入班级1的学生数量：'))
+
+#class2 = set()
+#for i in range(0,num2):
+#    name = input('输入学生%d姓名'%(i+1))
+ #   class2.add(name)
+
+#same = class1&class2
+#print("重名的学生")
+#for name in same:
+#    print(name)
+
+#price={'a':5,'b':6,'c':3,'d':4}
+
+#print("key --> value")
+#for key in price:
+#    print("%c-->%d"%(key,price[key]))
+#print("-----------------------------------------")
+   
+#n = int(input("请输入执行数量："))
+
+#sum_price = 0
+#for i in range(0,n):
+#    key = input('输入key 第%d 名称：' %(i+1))
+#    num = int(input('输入%d数量：'%(i+1)))
+#    
+#    if key in price:
+#        sum_price += price[key]*num
+        
+#print("总 %d"%(sum_price))
+
+v1 =True
+v2= False
+v3 = True
+print(v1)
+print(v2)
+print(int(v1))
+print(int(v2))
+
